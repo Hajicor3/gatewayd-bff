2711448301c7e206bea73b9a69a108127f30e751
Hajicor3 <gabriel.mirandawv@outlook.com>
2025-04-22 14:17:46 -0300

    Dcoumentando dos métodos com o swagger

--- src/main/java/com/example/gatewayBff/api/controllers/ApiLojaVirtualController.java
+++ src/main/java/com/example/gatewayBff/api/controllers/ApiLojaVirtualController.java
@@ -12,7 +12,9 @@ import com.example.gatewayBff.api.services.ApiClienteService;
 import com.example.gatewayBff.api.services.ApiFornecedoresService;
 import com.example.gatewayBff.api.services.ApiLojaVirtualService;
 import com.example.gatewayBff.api.services.ApiProdutosService;
-import feign.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +35,11 @@ public class ApiLojaVirtualController {
     private final ApiClienteService apiClienteService;
     private final ApiFornecedoresService apiFornecedoresService;
 
+    @Operation(description = "Salva um fornecedor no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salva o fornecedor no banco de dados"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos")
+    })
     @PostMapping("/fornecedor")
     public ResponseEntity<FornecedorResponse> criarFornecedor(@RequestBody FornecedorRequest fornecedor) {
         var fornecedorCriado = apiFornecedoresService.salvarFornecedor(fornecedor);
@@ -40,30 +47,54 @@ public class ApiLojaVirtualController {
         return ResponseEntity.created(uri).body(fornecedorCriado);
     }
 
+    @Operation(description = "Resgata um fornecedor do banco de dados pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um fornecedor"),
+            @ApiResponse(responseCode = "404", description = "Não existe fornecedor no id informado")
+    })
     @GetMapping("fornecedor/{id}")
     public ResponseEntity<FornecedorResponse> resgatarFornecedorPorId(@PathVariable Long id){
         var fornecedor = apiFornecedoresService.resgatarFornecedor(id);
         return ResponseEntity.ok().body(fornecedor);
     }
 
+    @Operation(description = "Retorna uma lista de Dto´s de todos os fornecedores do banco. este Dto contem apenas o nome dos fornecedores")
+    @ApiResponses(value = @ApiResponse(responseCode = "200",description = "Retorna uma lista de fornecedoresDto"))
     @GetMapping("/fornecedores")
     public ResponseEntity<List<FornecedorRequest>> resgatarTodosFornecedores() {
         var fornecedores = apiFornecedoresService.resgatarTodosFornecedores();
         return ResponseEntity.ok().body(fornecedores);
     }
 
+    @Operation(description = "Atualiza um fornecedor no banco de dados pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Efetua a atualização do fornecedor no banco de dados"),
+            @ApiResponse(responseCode = "404", description = "Não existe fornecedor no id informado"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos")
+    })
     @PutMapping("fornecedor/{id}")
     public ResponseEntity<Void> atualizarDadosFornecedorPorId(@PathVariable Long id, @RequestBody FornecedorRequest fornecedor){
         apiFornecedoresService.atualizarDadosFornecedor(id,fornecedor);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Deleta um fornecedor do banco de dados pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Efetua a exclusão do fornecedor"),
+            @ApiResponse(responseCode = "404", description = "Não existe fornecedor no id informado"),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos")
+    })
     @DeleteMapping("fornecedor/{id}")
     public ResponseEntity<Void> deletarFornecedor(@PathVariable Long id){
         apiFornecedoresService.deletarFornecedor(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Salva um produto no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salva o produto no banco de dados."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @PostMapping("/produto")
     public ResponseEntity<ProdutoResponse> criarProduto(@RequestBody ProdutoRequest produto) {
         var response = apiProdutosService.salvarProduto(produto);
@@ -72,12 +103,19 @@ public class ApiLojaVirtualController {
         return ResponseEntity.created(uri).body(response);
     }
 
+    @Operation(description = "Resgata um produto do banco de dados pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um produto."),
+            @ApiResponse(responseCode = "404", description = "Não existe produto no id informado.")
+    })
     @GetMapping(value = "produto/{id}")
     public ResponseEntity<ProdutoResponse> resgatarProdutoPorId(@PathVariable Long id) {
         var response = apiProdutosService.resgatarProduto(id);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(description = "Retorna uma lista de Dto´s de todos os produtos do banco.")
+    @ApiResponses(value = @ApiResponse(responseCode = "200",description = "Retorna uma lista de fornecedoresDto."))
     @GetMapping("/produtos")
     public ResponseEntity<List<ProdutoResponse>> resgatarTodosProdutos() {
         var response = apiProdutosService.resgatarTodosProdutos();
@@ -85,18 +123,35 @@ public class ApiLojaVirtualController {
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(description = "Atualiza um produto no banco de dados pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Efetua a atualização do produto no banco de dados."),
+            @ApiResponse(responseCode = "404", description = "Não existe produto no id informado."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @PutMapping("produto/{id}")
     public ResponseEntity<Void> atualizarDadosProdutoPorId(@PathVariable Long id, @RequestBody ProdutoResponse produto){
         apiProdutosService.atualizarDadosProduto(id,produto);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Deleta um produto do banco de dados pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Efetua a exclusão do produto."),
+            @ApiResponse(responseCode = "404", description = "Não existe produto no id informado."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @DeleteMapping("produto/{id}")
     public ResponseEntity<Void> deletarProdutoPorId(@PathVariable Long id){
         apiProdutosService.deletarProduto(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Salva um Cliente no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salva um cliente."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @PostMapping("/cliente")
     public ResponseEntity<ClienteResponse> registrarCliente(@RequestBody ClienteRequest cliente) {
         var response = apiClienteService.salvarCliente(cliente);
@@ -105,18 +160,34 @@ public class ApiLojaVirtualController {
         return ResponseEntity.created(uri).body(response);
     }
 
+    @Operation(description = "Resgata um cliente do banco de dados pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna um cliente."),
+            @ApiResponse(responseCode = "404", description = "Não existe cliente no id informado.")
+    })
     @GetMapping("/cliente/{id}")
     public ResponseEntity<ClienteResponse> resgatarClientePorId(@PathVariable Long id) {
         var response = apiClienteService.resgatarCliente(id);
         return ResponseEntity.ok().body(response);
     }
 
+    @Operation(description = "Atualiza um cliente no banco de dados pelo id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Efetua a atualização do cliente."),
+            @ApiResponse(responseCode = "404", description = "Não existe cliente no id informado."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @PutMapping("/cliente/{id}")
     public ResponseEntity<Void> atualizarClientePorId(@PathVariable Long id, @RequestBody ClienteRequest clienteAtualizado) {
         apiClienteService.atualizarCliente(id, clienteAtualizado);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(description = "Salva um Pedido de um cliente no banco de dados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Salva um pedido."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos.")
+    })
     @PostMapping("/cliente/pedido")
     public ResponseEntity<PedidoResponse> registrarPedidoCliente(@RequestBody PedidoRequest pedido) {
         var response = apiClienteService.registrarPedido(pedido);
